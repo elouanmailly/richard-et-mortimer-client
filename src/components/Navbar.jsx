1e1d2d5587b66aa457fb06eb133d0690e15bcc93
Elouan MAILLY <elouan@captag.fr>
2024-06-21 12:29:06 +0200

    chore: update dependencies and environment

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from 'react';
 import { useTranslation } from 'react-i18next';
 import { NavLink } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { solid } from '@fortawesome/fontawesome-svg-core/import.macro';
+import { faHeart, faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
 import darkModeContext from '../contexts/darkModeContext';
 
 function Navbar() {
@@ -13,7 +13,7 @@ function Navbar() {
     <nav className="relative w-full flex flex-wrap justify-between py-3 bg-gray-200 dark:bg-gray-800 text-black dark:text-white text-lg font-bold">
       <div className="pl-3">
         <NavLink to="/favorites">
-          <FontAwesomeIcon icon={solid('heart')} className="text-red-700" />
+          <FontAwesomeIcon icon={faHeart} className="text-red-700" />
         </NavLink>
       </div>
       <div>
@@ -24,8 +24,8 @@ function Navbar() {
       <div className="px-3 flex">
         <button type="button" onClick={() => setDarkMode(!darkMode)}>
           {
-            darkMode ? <FontAwesomeIcon icon={solid('moon')} />
-              : <FontAwesomeIcon icon={solid('sun')} />
+            darkMode ? <FontAwesomeIcon icon={faMoon} />
+              : <FontAwesomeIcon icon={faSun} />
           }
         </button>
       </div>
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import eslint from 'vite-plugin-eslint';
-// eslint-disable-next-line import/no-unresolved
-import macrosPlugin from 'vite-plugin-babel-macros';
 import { dependencies } from './package.json';
 
 function renderChunks(deps) {
@@ -15,9 +13,9 @@ function renderChunks(deps) {
 }
 
 export default defineConfig({
-  plugins: [react(), macrosPlugin(), eslint()],
+  plugins: [react(), eslint()],
   build: {
-    sourcemap: false,
+    sourcemap: true,
     rollupOptions: {
       output: {
         manualChunks: {
